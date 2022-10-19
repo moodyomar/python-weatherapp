9c23afa644d0a53e3e61df79c29523e32a779d6a
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-10-19 07:39:15 +0300

    change test in unit

--- tests/unit.py
+++ tests/unit.py
@@ -1,9 +1,13 @@
 import unittest
-from utils import get_daily_data
+# from utils import get_daily_data
+from utils import is_url_up,url
 
 class UnitTest(unittest.TestCase):
-    def testApiCall(self):
-        self.assertIsNotNone(get_daily_data(),"Api call return no values")
+    # def testApiCall(self):
+    #     self.assertIsNotNone(get_daily_data(),"Api call return no values")
+      
+    def testConnectionOk(self):
+        self.assertTrue(is_url_up(url),'Website is Not Up!')  
         
 if __name__ == '__main__':
    unittest.main()
