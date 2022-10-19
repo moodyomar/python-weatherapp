d8ad4d32f793372dca7e9f59f405dab9f126f8f3
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-10-19 07:28:01 +0300

    remove __init__ from tests dir

--- tests/static.py
+++ tests/static.py
@@ -4,6 +4,3 @@ import unittest
 class StaticTest(unittest.TestCase):
     def testCodeSyntax(self):
         self.assertEqual('test','test')
-        
-if __name__ == '__main__':
-   unittest.main()
